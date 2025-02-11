a181d1b540b37ba23d873612a19725632045dc2d
Troy Wiegand <troy@troywiegand.com>
2025-02-11 00:53:13 -0500

    Install SQLite for server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 require('dotenv').config();
+
+let sqlite3 = require('sqlite3');
+let sqlite = require('sqlite');
+// this is a top-level await 
+let db = null;
+(async () => {
+  // open the database
+  db = await sqlite.open({
+    filename: './score.sqlite',
+    driver: sqlite3.Database
+  })
+})()
 const express = require('express');
 const app = express();
 const PORT = 4000;
@@ -20,11 +32,13 @@ const games = {};
 const leaderboard = {};
 
 //Add this before the app.get() block
-socketIO.on('connection', (socket) => {
+socketIO.on('connection', async (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
+    players = await db.all('SELECT player FROM scores');
+    socketIO.emit('players', players.map(x=>x.player));
 
-    socket.on('message', (data) => {
-        socketIO.emit('messageResponse', data.text);
+      socket.on('message', (data) => {
+        socketIO.emit('messageResponse', data.text); 
       });
 
       socket.on('game', (data) => {
@@ -40,19 +54,23 @@ socketIO.on('connection', (socket) => {
         }
       });
 
-      socket.on('submitGame', (data)=>{
+      socket.on('submitGame', async (data)=>{
         console.log(data)
         const tempScore = JSON.parse(JSON.stringify(data.score));
+        mostRecentScore = await db.all('SELECT * FROM scores');
         tempScore.sort((x,y)=>y.score-x.score)
         console.log(tempScore)
-        tempScore.forEach((person,i) => {
+        tempScore.forEach(async (person,i) => {
           if(!!!leaderboard[person.name]){
             leaderboard[person.name]= {firstName: person.name, overallScore: tempScore.length-i, gameList: [{game: data.scoredGame, rank: i}]};
           } else {
             leaderboard[person.name].overallScore = (leaderboard[person.name].overallScore||0)+tempScore.length-i;
             leaderboard[person.name].gameList= [...leaderboard[person.name].gameList, {game: data.scoredGame, rank: i}];
           }
+          let dbEntry = mostRecentScore.find(x=>x.player===person.name);
+          await db.exec(`UPDATE scores SET score=${dbEntry.score+tempScore.length-i} WHERE player="${dbEntry.player}"`);
         });
+
         socketIO.emit('leaderboardUpdate',Object.values(leaderboard).sort((x,y)=>y.overallScore-x.overallScore))
       })
 
